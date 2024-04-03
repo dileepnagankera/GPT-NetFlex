1d3f7468c4a6af7a8eb520842ac5dae2251a3a76
dileepnagankera <dileepnagankera@gmail.com>
2024-04-03 14:01:01 +0530

    Completed Form Validation And autantication And Deploy

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHhawZJMnnIoQ5AVcjBo646pu5qYk_1L0",
+  authDomain: "gpt-netflex.firebaseapp.com",
+  projectId: "gpt-netflex",
+  storageBucket: "gpt-netflex.appspot.com",
+  messagingSenderId: "862248559350",
+  appId: "1:862248559350:web:4850e00801e815ca9dc2d7",
+  measurementId: "G-H5H2EJD5ZQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
