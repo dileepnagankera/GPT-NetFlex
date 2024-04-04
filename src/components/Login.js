fb150fc343bb39f6e25687a184eb981e2208feb1
dileepnagankera <dileepnagankera@gmail.com>
2024-04-04 18:01:56 +0530

    We did form validation and authentiction Form Logout And login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,97 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidaData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
+  const navigate=useNavigate()
+  const dispatch=useDispatch()
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
 
+  const name =useRef(null)
   const email = useRef(null);
   const password = useRef(null);
-  const name = useRef(null);
+
   const handleButtonClick = () => {
     //Validata the form Data
     const message = checkValidaData(
-      name.current.value,
-      email.current.value,
+      email.current.value,                                                              
       password.current.value
     );
 
     setErrorMessage(message);
 
-    //Sign / Sign Up
-  };
+    if (message) return;
+    //Sign In   Sign Up Logic
 
-  ;
+    if (!isSignInForm) {
+      //Sign Up Logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/12811851?v=4",
+          })
+            .then(() => {
+              // Profile updated!
+              const { uid, email, displyName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displyName: displyName,
+                  photoURL: photoURL,
+                })
+              );
+               navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              setErrorMessage(error.message)
+            });
+         
+          
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //Sign In Logic
+
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user, "DDDDD");
+           navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(`${errorCode}-${errorMessage}`);
+        });
+    }
+  };
 
   const toggleSingInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -46,13 +115,13 @@ const Login = () => {
         </h1>
         {!isSignInForm && (
           <input
-            ref={name}
+          ref={name}
             type="name"
             placeholder="Full Name"
             className="p-4 my-2 w-full bg-gray-700"
           />
         )}
-        <input
+        <input  
           ref={email}
           type="text"
           placeholder="Email Address"
